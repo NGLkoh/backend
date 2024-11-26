5c0927d446249a40a9f2f7a8e8791f7804d7b2e7
NGLkoh <nigelkoh03@gmail.com>
2024-11-26 14:42:52 +0800

    update

--- src/events/events.module.service.ts
+++ src/events/events.module.service.ts
@@ -34,7 +34,6 @@ export class EventService {
 		const result: any =  await this.eventModel.find({ '_id': categorySearchResquestDto.ids}).exec()
 		return { status: 200, message: result.length >= 1  ? 'true' : 'false', result : result};
 	}
-
     async updateById(categoryUpdateResquestDto: CategoryUpdateResquestDto): Promise<any> {
          let newId = new Types.ObjectId(categoryUpdateResquestDto.ids)
 	 	const result: any =  await this.eventModel.updateOne( { _id: newId }, { $set: { title: categoryUpdateResquestDto.title } },{ upsert: true }).exec()
