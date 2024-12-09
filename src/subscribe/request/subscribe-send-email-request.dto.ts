3bdb0781c112543c7ce0aeab831dc4662c97c66f
NGLkoh <nigelkoh03@gmail.com>
2024-12-09 16:18:06 +0800

    update

--- src/subscribe/request/subscribe-send-email-request.dto.ts
+++ src/subscribe/request/subscribe-send-email-request.dto.ts
@@ -1,5 +1,4 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { HtmlContext } from "next/dist/server/future/route-modules/app-page/vendored/contexts/entrypoints";
 
 export class SubscribeSendMailResquestDto {
 	@ApiProperty()
